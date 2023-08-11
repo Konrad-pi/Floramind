398350043a6e722a6208ab03184f754ce374b008
Ntsoumou <manuel.ntsoumou@hotmail.com>
2023-08-11 14:22:15 +0200

    API_KEY changes

--- LLM/clustering.py
+++ LLM/clustering.py
@@ -1,9 +1,4 @@
-import config
 import os
 import openai
 
-openai.organization = "org-mRQBmUYHKZKOcTfdAI7JUwa1"
-openai.api_key = os.getenv(config.private_key)
-openai.Model.list()
-
-print(config.private_key)
+API_KEY = open("API_KEY", "r").read()
